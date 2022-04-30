5e47fa1f485bf0e8d78f70b10195254e930ce6ad
shed4329 <74101498+shed4329@users.noreply.github.com>
2022-04-30 22:30:37 +0800

    Add files via upload

---
+++ RunMe.bat
@@ -0,0 +1,44 @@
+
+C:
+cd..
+cd..
+cd..
+cd 3000soft
+ren "Red Spider" "Blue Spider"
+taskkill /im REDAgent.exe /f
+ren "Red Spider" "Blue Spider"
+taskkill /im RSpider.exe /f
+ren "Red Spider" "Blue Spider"
+
+cd..
+cd Program Files
+cd 3000soft
+ren "Red Spider" "Blue Spider"
+taskkill /im REDAgent.exe /f
+ren "Red Spider" "Blue Spider"
+taskkill /im RSpider.exe /f
+ren "Red Spider" "Blue Spider"
+
+cd..
+cd..
+cd Program Files (x86)
+cd 3000soft
+ren "Red Spider" "Blue Spider"
+taskkill /im REDAgent.exe /f
+ren "Red Spider" "Blue Spider"
+taskkill /im RSpider.exe /f
+ren "Red Spider" "Blue Spider"
+
+F:
+ntsd -c q -pn studentmain.exe
+taskkill /im studentmain.exe
+
+ntsd -c q -pn REDAgent.exe
+C:
+cd..
+cd..
+cd..
+cd 3000soft
+ren "Red Spider" "Blue Spider"
+
+pause
